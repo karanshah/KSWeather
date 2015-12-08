2593743c41f6a7cb553e9fd3ae8fb388444055a0
Karan Shah <shah.karan@gmail.com>
2015-12-07 17:10:26 -0700

    Added a custom circle view class

---
+++ UIComponenets/UIComponenets.h
@@ -0,0 +1,19 @@
+//
+//  UIComponenets.h
+//  UIComponenets
+//
+//  Created by Karan Shah on 12/7/15.
+//  Copyright © 2015 Karan Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIComponenets.
+FOUNDATION_EXPORT double UIComponenetsVersionNumber;
+
+//! Project version string for UIComponenets.
+FOUNDATION_EXPORT const unsigned char UIComponenetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIComponenets/PublicHeader.h>
+
+
